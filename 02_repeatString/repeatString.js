7a48664d27a8378dec71b51bb04a43075a4e8bde
giantaw1 <adrian.warner@gmail.com>
2024-08-15 17:32:50 -0400

    Add loop solution and test debuggin process.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeatedStr = "";
+    for (let i = 1; i <= num; i++) {
+        repeatedStr += str;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
