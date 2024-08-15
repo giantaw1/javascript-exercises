5149712d221185f101fa62ec77d058a785935c34
giantaw1 <adrian.warner@gmail.com>
2024-08-15 18:08:46 -0400

    Add solution code and pass all tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(str) {
+    let strArr = str.split("");
+    let reverseArr = [];    
+    for (char of strArr) {
+      reverseArr.unshift(char);
+    }
+    return reverseArr.join("");
+}
 
 // Do not edit below this line
 module.exports = reverseString;
