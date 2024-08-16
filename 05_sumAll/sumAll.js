0af5277f1983ad2c856f333f3f87126ea0851d98
giantaw1 <adrian.warner@gmail.com>
2024-08-16 12:12:57 -0400

    Add code to pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let finalSum = 0;
+    if (a > b) {
+      for (let i = b; i <= a; i++) {
+        finalSum += i;
+      }
+    } else if (a < 0 || b < 0) {
+      return "ERROR";
+    } else if (a !== parseInt(a) || b !== parseInt(b)) {
+      return "ERROR";
+    } else {
+        for (let i = a; i <= b; i++) {
+          finalSum += i;
+      }
+    }
+  return finalSum;
 };
 
 // Do not edit below this line
