a766e2b7266cc04c5f88c99182494ad59419a17e
giantaw1 <adrian.warner@gmail.com>
2024-08-16 15:24:13 -0400

    Add code to pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  const toCelsius = (deg - 32) * 5 / 9;
+  if (toCelsius !== parseInt(toCelsius)){
+    return +toCelsius.toFixed(1);
+  } else {
+    return toCelsius;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  const toFahrenheit = deg * 9 / 5 + 32;
+  if (toFahrenheit !== parseInt(toFahrenheit)){
+    return +toFahrenheit.toFixed(1);
+  } else {
+    return toFahrenheit;
+  }
 };
+
 
 // Do not edit below this line
 module.exports = {
