d74a129d3b5d7bbdae07ab4bde1da6820f4f65cb
giantaw1 <adrian.warner@gmail.com>
2024-09-05 08:46:22 -0700

    Add code to pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const clean = (str) => str.toLowerCase().replace(/[\W_]/g, '');
+    const cleanStr = clean(str);
+    const reverseStr = cleanStr.split('').reverse().join('');
+    return reverseStr === cleanStr;
 };
 
 // Do not edit below this line
