a528cedf1b90e000c168a2fff09fd7c9b60d3d47
giantaw1 <adrian.warner@gmail.com>
2024-09-05 13:41:08 -0700

    Add code to pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n === 0) return 0;
+    if (n === "0") return 0;
+    if (n === 1) return 1;
+    if (n < 0) return 'OOPS';
+    let num = parseInt(n); 
+    let prev1 = 0, prev2 = 1;
+    for (let i = 2; i <= num; i++) {
+      let current = prev1 + prev2;
+      prev1 = prev2;
+      prev2 = current;
+    }
+  
+    return prev2;
 };
 
 // Do not edit below this line
