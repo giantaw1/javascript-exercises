ecd60baaa876e9c665c2cb9953100a6cd9ea1f0d
giantaw1 <adrian.warner@gmail.com>
2024-09-09 14:05:56 -0400

    Add code to pass some tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        // Calculate the age of the current person and the oldest found so far
+        const currentAge = person.yearOfDeath - person.yearOfBirth;
+        const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        // Return the person with the greater age
+        return currentAge > oldestAge ? person : oldest;
+      });
 };
 
 // Do not edit below this line
